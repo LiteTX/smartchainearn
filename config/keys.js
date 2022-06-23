2c865e73bab71688201a35dcb903bbb5d324c9b6
Benjamin Dev <Benjamin_dev@Benjamins-MacBook-Pro.local>
2022-06-23 13:38:23 +0100

    all

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword3 = 'mongodb://localhost:27017/mern';
+dbPassword = 'mongodb+srv://coinworld:coinworld@cluster0.0expj.mongodb.net/<dbname>?retryWrites=true&w=majority'
+
+module.exports = {
+    mongoURI: dbPassword
+};
